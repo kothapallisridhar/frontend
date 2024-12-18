0ff0ddae42e1a0ba7dc8f7318cd44979ed9ae383
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-12-18 13:39:25 +0530

    counter app

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,17 +1,29 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  let [counter, setCounter] = useState(0);
+  let stock = 10;
 
   return (
     <>
-      <div className='Wrapper'>
-       <h1>Change the background color</h1>
-       <input type="color" name="color" id="color" onChange={(e)=>{
-          document.body.style.background = e.target.value;
-       }}/>
-      </div>
+    <h2>React Counter App</h2>
+    <div className='Wrapper'>
+      <button className='minus' disabled={counter === 0} onClick={()=>{
+        if(counter > 0) {
+          setCounter(counter - 1);
+        }
+      }}>-</button>
+      <p className='counter'>{counter}</p>
+      <button className='plus' disabled={counter === stock} onClick={()=>{
+        if(counter < 10) {
+          setCounter(counter + 1);
+        }
+      }}>+</button>
+    </div>
     </>
   )
+  
 }
 
 export default App
