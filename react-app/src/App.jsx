8a2d56ca20f184c05ec88f305363f2bbbc9d21e5
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-01-04 12:16:43 +0530

    react app

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,26 +1,32 @@
 import './App.css'
 import { useState } from 'react'
+import Product from './components/Product';
 
 function App() {
   let [counter, setCounter] = useState(0);
   let stock = 10;
+  let prod = {
+    id: 123,
+    name: "Mac Book Pro",
+    price: 300000,
+    nprice: 289999
+  }
 
   return (
     <>
-    <h2>React Counter App</h2>
-    <div className='Wrapper'>
-      <button className='minus' disabled={counter === 0} onClick={()=>{
-        if(counter > 0) {
-          setCounter(counter - 1);
-        }
-      }}>-</button>
-      <p className='counter'>{counter}</p>
-      <button className='plus' disabled={counter === stock} onClick={()=>{
-        if(counter < 10) {
-          setCounter(counter + 1);
-        }
-      }}>+</button>
+    <h2>TODO App</h2>
+    <div>
+      <input></input>
+      <button>Add</button>
     </div>
+    <div>
+      <ul>
+        <li>Learn React</li>
+        <li>Learn Redux</li>
+        <li>Learn Angular</li>
+      </ul>
+    </div>
+    
     </>
   )
   
