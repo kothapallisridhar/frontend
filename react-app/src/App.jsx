b7a882f73c2403751b65b292bca4c3f873721326
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-04-23 21:30:57 +0530

    using map()

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,12 +3,18 @@ import User from './User'
 import UserTwo from './UserTwo'
 
 function App() {
+
+  let users = [{id: 24, name: 'Alen', age: 23, place: 'Munich'},
+    {id: 25, name: 'Glen', age: 24, place: 'Leipzig'},
+    {id: 26, name: 'Ben', age: 27, place: 'Bremen'}
+  ]
+
   return (
     <>
-      <User name='Alice' age='25' place='Bangalore'/>
-      <User name='Harry' age='24' place='Hyderabad'/>
-      <User name='Lene' age='26' place='Chennai'/>
-      <UserTwo name='Messi' age='38'/>
+      {users.map(user => {
+        return <User key={user.id} id={user.id} name={user.name} age={user.age} place={user.place}/>
+      })}
+      {/* <UserTwo name='Messi' age='38'/> */}
     </>
   )
 }
--- react-app/src/User.jsx
+++ react-app/src/User.jsx
@@ -1,15 +1,15 @@
 
 function User(props) {
-    const {name, age, place} = props
+    const {id, name, age, place} = props
 
     console.log("props: ", props)
     return(
-        <>
-        <h1>User name: {name} </h1>
-        <h2>Age: {age}</h2>
-        <h2>Place: {place}</h2>
-        
-        </>
+        <div style={{border: '2px solid skyblue', width: 300, margin: 20}}>
+            <h1>Id: {id}</h1>
+            <h2>Name: {name}</h2>
+            <h2>Age: {age}</h2>
+            <h2>Place: {place}</h2>
+        </div>
     )
 }
 
