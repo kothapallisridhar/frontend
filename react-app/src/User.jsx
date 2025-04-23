57769ef3333e2b640ec6b59589c96fd0f3cab782
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-04-23 21:35:20 +0530

    using arrow function

--- react-app/src/User.jsx
+++ react-app/src/User.jsx
@@ -1,4 +1,4 @@
-
+/*
 function User(props) {
     const {id, name, age, place} = props
 
@@ -12,5 +12,22 @@ function User(props) {
         </div>
     )
 }
+    */
+
+// using arrow function
+let User = (props) => {
+    const {id, name, age, place} = props
+
+    console.log("props: ", props)
+    return(
+        <div style={{border: '2px solid skyblue', width: 300, margin: 20}}>
+            <h1>Id: {id}</h1>
+            <h2>Name: {name}</h2>
+            <h2>Age: {age}</h2>
+            <h2>Place: {place}</h2>
+        </div>
+    )
+}
+
 
 export default User;
