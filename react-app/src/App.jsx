250fd6c2b0a2c919215ef2d1e2ece5d610c13f77
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-04-23 23:06:45 +0530

    event handling

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,6 +2,10 @@ import './App.css'
 import User from './User'
 import UserTwo from './UserTwo'
 
+// function displayMessage() {
+//   alert("Hello")
+// }
+
 function App(props) {
   console.dir(props)
   let users = [{id: 24, name: 'Alen', age: 23, place: 'Munich'},
@@ -9,12 +13,20 @@ function App(props) {
     {id: 26, name: 'Ben', age: 27, place: 'Bremen'}
   ]
 
+  function displayMessage(num, e) {
+    alert("button clicked! " + num)
+    console.log(e.target)
+    console.dir(e.target)
+  }
+
   return (
     <>
     <h2 style={{display: 'flex', gap: 20, color: 'orange'}}>{props.children}</h2>
       {users.map(user => {
         return <User key={user.id} id={user.id} name={user.name} age={user.age} place={user.place}/>
       })}
+      <button onClick={(e) => displayMessage(10, e)}>Click</button>
+      <input type="text" onKeyUp={(e) => displayMessage(10, e)}/>
       {/* <UserTwo name='Messi' age='38'/> */}
     </>
   )
