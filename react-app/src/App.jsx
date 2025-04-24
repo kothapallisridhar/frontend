68ff49e71dd8fd16fb4c48391f8937111770b642
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-04-24 11:00:20 +0530

    functional component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,27 +1,15 @@
 import './App.css'
-import User from './User'
-import UserTwo from './UserTwo'
-import { useState } from 'react'
-import Product from './Product'
+import { Greet } from './components/Greet'
 
-// function displayMessage() {
-//   alert("Hello")
-// }
 
 function App(props) {
-  const [count, setCount] = useState(0)
 
   console.dir(props)
 
   return (
-    <>
-    <h2 style={{display: 'flex', gap: 20, color: 'orange'}}>{props.children}</h2>
-      <label htmlFor="">Count: {count}</label>
-      <br />
-      <button onClick={() => setCount(count + 1)}>Count</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-      <Product/>
-    </>
+    <div className='app'>
+      <Greet/>
+    </div>
   )
 }
 
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,20 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
 import App from './App.jsx'
-import UserTwo from './UserTwo.jsx'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
   <>
-    <App>
-      This is App Component
-      <button>Like</button>
-      <button>Share</button>
-      <button>Subscribe</button>
-    </App>
-    <UserTwo/>
+    <App/>
   </>
   
 )
