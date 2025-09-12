329d85558a3b73db386f4003576c54a9cf4ec494
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-09-12 12:24:15 +0530

    count app functional component

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import { Layout } from './components/Layout'
+import React, { useState } from "react";
+import "./App.css"
 
 export const App = () => {
-  const person = {
-    id: 1,
-    fname: "Sridhar",
-    lname: "Kothapalli",
-    desig: "Architect" 
-  }
+  const [count, setCount] = useState(0);
+
+  const decrementHandler = () => {
+    setCount(count - 1);
+  };
+  const incrementHandler = () => {
+    setCount(count + 1);
+  };
+  const resetHandler = () => {
+    setCount(0);
+  };
   return (
-    <div>
-      <Layout name={"Sridhar"} person={person}/>
+    <div className="container">
+      <div>
+        <h1>Count: {count}</h1>
+      </div>
+      <div className="button-container">
+        <button onClick={decrementHandler}>{"<<"}</button>
+        <button onClick={resetHandler}> {"Reset"}</button>
+        <button onClick={incrementHandler}> {">>"}</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
