606ec8412d8e73de06f184840c384a4797d01fd3
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-09-13 14:00:56 +0530

    install axios

--- reactapp/src/components/Posts.js
+++ reactapp/src/components/Posts.js
@@ -1,15 +1,29 @@
-import React, { useEffect, useState } from 'react'
-const BASE_URL = 'https://jsonplaceholder.typicode.com/posts'
+import React, { useEffect, useState } from "react";
+const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
 
 export const Posts = () => {
-  const [data, setData] = useState([]);  
+  const [data, setData] = useState([]);
   useEffect(() => {
-    fetch(BASE_URL).then(res => res.json())
-    .then(data => console.log(data))
-  }, [])
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
   return (
     <div>
-       <h1>Posts</h1> 
+      <h1>Posts</h1>
+      <table>
+        {data.map((element) => {
+          return (
+            <>
+              <tr key={element.id}>
+                <td>{element.id}</td>
+                <td>{element.title}</td>
+                <td>{element.body}</td>
+              </tr>
+            </>
+          );
+        })}
+      </table>
     </div>
-  )
-}
+  );
+};
