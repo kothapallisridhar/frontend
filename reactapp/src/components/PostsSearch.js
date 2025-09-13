06a25e7de98cc45ff7135b0d5faec4e8a8b2d768
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-09-13 23:28:33 +0530

    implement search

--- reactapp/src/components/PostsSearch.js
+++ reactapp/src/components/PostsSearch.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./posts.css";
+
+const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
 
 export const PostsSearch = () => {
+  const [search, setSearch] = useState("");
+  const [posts, setPosts] = useState([]);
+  const getApiData = async () => {
+    const data = await axios.get(BASE_URL);
+    setPosts(data.data);
+  };
+  useEffect(() => {
+    getApiData();
+  }, []);
   return (
-    <div>
-        <h1>Welcome to Posts</h1>
+    <div className="my-container">
+      <input type="search" onChange={(e) => setSearch(e.target.value)} />
+      <h1>Welcome to Posts</h1>
+      <table id="posts">
+        <thead>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Body</th>
+        </thead>
+        <tbody>
+          {" "}
+          {posts
+            .filter(
+              (element) =>
+                element.title.toLowerCase().includes(search) ||
+                element.body.toLowerCase().includes(search)
+            )
+            .map((element) => {
+              return (
+                <tr key={element.id}>
+                  <td>{element.id}</td> <td>{element.title}</td>{" "}
+                  <td>{element.body}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
