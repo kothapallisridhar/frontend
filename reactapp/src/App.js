7e990411188b2d573537af53a8a580702163cd11
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-09-22 21:33:01 +0530

    uuid

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,16 +1,7 @@
 import React from 'react'
-import { InputData } from './components/InputData';
-import './App.css'
 
 export const App = () => {
-
   return (
-    <>
-      <div>
-        <h2>useRef hook</h2>
-      </div>
-      <hr></hr>
-      <InputData/>
-    </>
+    <div>App</div>
   )
 }
