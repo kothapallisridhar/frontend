bcc9cc05fe9f5657a5116aa214bf03e71f91f663
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-04 09:10:37 +0530

    string is primitive type

--- javascript/reference-types.js
+++ javascript/reference-types.js
@@ -1,8 +1,12 @@
 
 var ar = [10, 20];
 var br = [10, 20]
-console.log(ar == br);
+console.log(ar == br); // array is reference(non-primitive) data type
 
 var a = 10;
 var b = 10;
 console.log(a==b);
+
+var x = "sai";
+var y = "sai";
+console.log(x == y); // string is value(primitive) data type
