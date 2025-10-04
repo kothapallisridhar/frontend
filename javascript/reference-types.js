dab185275e70120de80414e97268f4eb0f4355d3
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-04 09:18:49 +0530

    array reference

--- javascript/reference-types.js
+++ javascript/reference-types.js
@@ -1,11 +1,15 @@
 
 var ar = [10, 20];
 var br = [10, 20]
+// heap memory
 console.log(ar == br); // array is reference(non-primitive) data type
+var cr = ar;
+console.log(ar == cr);
+
 
 var a = 10;
 var b = 10;
-console.log(a==b);
+console.log(a==b); // constant pool (primitive types)
 
 var x = "sai";
 var y = "sai";
