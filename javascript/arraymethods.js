205eaa4c1f4377ff2e6234953b7a5af5a4152aff
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-05 13:36:02 +0530

    array methods

---
+++ javascript/arraymethods.js
@@ -0,0 +1,16 @@
+
+var ar = [10, 20, 38, 19, 59]
+
+console.log(ar)
+
+ar.push(33);
+console.log(ar)
+
+ar.unshift(5);
+console.log(ar)
+
+ar.pop();
+console.log(ar);
+
+ar.shift();
+console.log(ar)
