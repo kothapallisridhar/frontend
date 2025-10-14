389b66996ddb00bbbefae3c53740f8b4ace68d75
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-14 16:30:04 +0530

    rest operator

--- javascript/destructuring.js
+++ javascript/destructuring.js
@@ -47,12 +47,19 @@ Object.freeze(ob2)
 ob2 = {...ob2, country: 'Germany'}
 var ob3 = {...ob1, ...ob2}
 console.log(ob3)
-
+/*
 var ar = [12, 14]
 var br = [15, 17]
+ar = [...ar, 33]
 
 var cr = [...ar, ...br]
 console.log(cr)
 
 console.log(ar)
 console.log(...ar)
+*/
+
+var ar = [12,14, 15, 16, 17, 19, 22, 23, 24, 25]
+
+var [a, b, ...c] = ar; // ...c => rest variable
+console.log(a, b, c)
