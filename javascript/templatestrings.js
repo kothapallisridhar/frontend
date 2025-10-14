b755ee170009663ba700fcce50e1f3e42ef29a69
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-14 22:08:31 +0530

    pattern

--- javascript/templatestrings.js
+++ javascript/templatestrings.js
@@ -1,11 +1,16 @@
 console.log('template strings')
-
+/*
 var a = +prompt()
 var b = +prompt()
 
 console.log(`the addition of ${a} and ${b} is ${a + b}`);
-
+*/
 var mstr = `Hello, Sridhar!
 Good Morning!!`
 
-console.log(mstr);            
+var pattern = `    *
+  * * *
+* * * * *`
+
+console.log(mstr);  
+console.log(pattern)          
