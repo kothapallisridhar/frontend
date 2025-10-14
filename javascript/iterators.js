e67435417512a8cdd321794e3624348b31c86792
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-14 08:36:14 +0530

    iterators

--- javascript/iterators.js
+++ javascript/iterators.js
@@ -1,6 +1,12 @@
 var ar = [12, 29, 18, 4]
 
-for(var k of ar) {
+for(let k of ar) {
     console.log("hello", k)
 }
-console.log(k)
+//console.log(k)
+
+var h2elements = document.getElementsByTagName('h2');
+console.log(h2elements)
+
+var c = document.querySelectorAll('.abc');
+console.log(c)
