e9a7682e4eb8a2d0d119293c754db70cdff6d897
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-15 00:02:49 +0530

    default parameters

--- javascript/templatestrings.js
+++ javascript/templatestrings.js
@@ -28,3 +28,17 @@ var student = {
 }
 
 console.log(student)
+
+function add(a=35, b=9, c=7) {
+    return a+b+c
+}
+console.log(add(10, 20, 30))
+console.log(add(10, 20))
+
+var ar = [10, 20, 5]
+var [a, b, c, d, e] = ar;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
