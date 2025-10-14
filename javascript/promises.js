b1fa4f8f7d98d4911e9df3484e9d374d71fea5e1
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-14 10:18:56 +0530

    promise

--- javascript/promises.js
+++ javascript/promises.js
@@ -1,10 +1,22 @@
-
+/*
 var http = new XMLHttpRequest();
+console.log(http.responseText)
 console.log(http)
 http.open('get', 'https://fakestoreapi.com/products')
 http.send()
 console.log(http.responseText)
+console.log(http.responseText)
 http.onloadend = function() {
-    console.log('on load end', http.responseText);
+    console.log('on load end: ', http.responseText);
 }
 console.log('Hello');
+*/
+
+var p = fetch('https://restcountries.com/v3.1/name/deutschland')
+            .then(function() {
+                console.log("success")
+            })
+            .catch(function() {
+                console.log('check the input or request details')
+            })
+console.log(p);
