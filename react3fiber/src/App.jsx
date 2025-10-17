082d96d22d466f4873ccb48f4da2d2a135905ca1
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-17 23:23:52 +0530

    random colors using hsl()

--- react3fiber/src/App.jsx
+++ react3fiber/src/App.jsx
@@ -14,9 +14,9 @@ function App() {
       <button onClick={toggleClicker}>{hasClicker ? 'Hide Clicker' : 'Show Clicker'}</button>
       {hasClicker && 
       <>
-      <Clicker keyName='countA' color='crimson'/>
-      <Clicker keyName='countB' color='cyan'/>
-      <Clicker keyName='countC' color='darkOrchid'/>
+      <Clicker keyName='countA' color={`hsl(${Math.random() * 360}deg, 100%, 70%)`}/>
+      <Clicker keyName='countB' color={`hsl(${Math.random() * 360}deg, 100%, 70%)`}/>
+      <Clicker keyName='countC' color={`hsl(${Math.random() * 360}deg, 100%, 70%)`}/>
       </>}
     </>
   )
