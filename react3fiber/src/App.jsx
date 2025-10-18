03f9b94e8f129556563e6a3bb2871a03189f5246
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-18 19:03:06 +0530

    r3f getting started

--- react3fiber/src/App.jsx
+++ react3fiber/src/App.jsx
@@ -1,31 +1,12 @@
-import { useState } from 'react';
-import Clicker from './Clicker.jsx'
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience'
 
-function App({children}) {
-
-  const [hasClicker, setHasClicker] = useState(true);
-  const [count, setCount] = useState(0)
-
-  function toggleClicker() {
-    setHasClicker(!hasClicker);
-  }
-
-  const increment = () => {
-    setCount(count + 1)
-  }
-
+function App() {
   return (
-    <>
-      {children}
-      <div>Total count: {count}</div>
-      <button onClick={toggleClicker}>{hasClicker ? 'Hide Clicker' : 'Show Clicker'}</button>
-      {hasClicker && 
-      <>
-      <Clicker increment={increment} keyName='countA' color={`hsl(${Math.random() * 360}deg, 100%, 70%)`}/>
-      <Clicker increment={increment} keyName='countB' color={`hsl(${Math.random() * 360}deg, 100%, 70%)`}/>
-      <Clicker increment={increment} keyName='countC' color={`hsl(${Math.random() * 360}deg, 100%, 70%)`}/>
-      </>}
-    </>
+      <Canvas>
+        <Experience/>
+      </Canvas>
   )
 }
 
--- react3fiber/src/index.jsx
+++ react3fiber/src/index.jsx
@@ -4,10 +4,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App>
-      <h1>React App</h1>
-      <h2>Fancy name</h2>
-      <h3>Sub title</h3>
-    </App>
+    <App/>
   </StrictMode>,
 )
