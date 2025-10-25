bec7a65379caeb8927b46d27d7f9a4c3eb689434
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-25 16:03:30 +0530

    bootstrap border for table data

--- rb49app/src/products/Products.jsx
+++ rb49app/src/products/Products.jsx
@@ -12,18 +12,18 @@ function Products() {
   }, []);
 
   return (
-    <div className="border border-5 p-2  m-2 border-info">
+    <div className="border border-3 p-2  m-2 border-info">
       <h1>Products Length: {products.length}</h1>
       <table className="table">
         {products.map((product) => {
           return (
             <tr className="border border-1 border-info">
-              <td>{product.id}</td>
-              <td>{product.title}</td>
-              <td>{product.description}</td>
-              <td>{product.price}</td>
-              <td>{product.rating.rate}</td>
-              <td>
+              <td className="border">{product.id}</td>
+              <td className="border">{product.title}</td>
+              <td className="border">{product.description}</td>
+              <td className="border">{product.price}</td>
+              <td className="border">{product.rating.rate}</td>
+              <td className="border">
                 <img width="50px" src={product.image} />
               </td>
             </tr>
