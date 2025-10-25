fd4452d70a9f8984df3b8111897a0af0330ef3af
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-10-25 16:07:07 +0530

    add tbody

--- rb49app/src/products/Products.jsx
+++ rb49app/src/products/Products.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function Products() {
   var [products, setProducts] = useState([]);
-  console.log(products);
+
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
@@ -15,20 +15,22 @@ function Products() {
     <div className="border border-3 p-2  m-2 border-info">
       <h1>Products Length: {products.length}</h1>
       <table className="table">
-        {products.map((product) => {
-          return (
-            <tr className="border border-1 border-info">
-              <td className="border">{product.id}</td>
-              <td className="border">{product.title}</td>
-              <td className="border">{product.description}</td>
-              <td className="border">{product.price}</td>
-              <td className="border">{product.rating.rate}</td>
-              <td className="border">
-                <img width="50px" src={product.image} />
-              </td>
-            </tr>
-          );
-        })}
+        <tbody>
+          {products.map((product) => {
+            return (
+              <tr key={product.id} className="border border-1 border-info">
+                <td className="border">{product.id}</td>
+                <td className="border">{product.title}</td>
+                <td className="border">{product.description}</td>
+                <td className="border">{product.price}</td>
+                <td className="border">{product.rating.rate}</td>
+                <td className="border">
+                  <img width="50px" src={product.image} />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
