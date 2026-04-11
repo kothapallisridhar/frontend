ae3f2d1fbb746a52e3613868abd920a159844cc5
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2026-04-11 12:33:38 +0530

    refactor

--- react3fiber/src/App.jsx
+++ react3fiber/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
+import CustomObject from "./CustomObject";
 
 function App() {
   return (
     <Canvas>
       <Experience />
+      <CustomObject/>
     </Canvas>
   );
 }
